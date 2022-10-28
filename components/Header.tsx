15d7a39211111ff40a4f23e7ce6a6568b6db437e
odysseaspapadimas <odysseas.patra@gmail.com>
2022-10-28 14:28:03 +0300

    added mantine, tailwind and dark/light mode toggle button

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import { ActionIcon, Button, Group, Text, useMantineColorScheme } from '@mantine/core'
+import { IconMoonStars, IconSun } from '@tabler/icons';
+
+const Header = () => {
+
+    const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+    return (
+        <header>
+            <Group position="center" my="xl">
+                <ActionIcon
+                    onClick={() => toggleColorScheme()}
+                    size="lg"
+                    sx={(theme) => ({
+                        backgroundColor:
+                            theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+                        color: theme.colorScheme === 'dark' ? theme.colors.yellow[4] : theme.colors.blue[6],
+                    })}
+                >
+                    {colorScheme === 'dark' ? <IconSun size={18} /> : <IconMoonStars size={18} />}
+                </ActionIcon>
+            </Group>
+        </header>
+    )
+}
+export default Header
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ColorScheme, ColorSchemeProvider, Container, MantineProvider, useMantineColorScheme } from '@mantine/core';
+import { useHotkeys, useLocalStorage } from '@mantine/hooks';
+import Header from '../components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'light',
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: colorScheme,
+        }}
+      >
+        <Container size="lg">
+          <Header />
+          <Component {...pageProps} />
+        </Container>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  )
 }
