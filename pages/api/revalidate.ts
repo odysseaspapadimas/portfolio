fe516b77bc9c0cdc55fdf7b6ac494888eb0ffa97
odysseaspapadimas <odysseas.patra@gmail.com>
2022-11-01 00:08:05 +0200

    removed secret checking

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,22 +1,14 @@
-import { isValidRequest } from "@sanity/webhook"
 import type { NextApiRequest, NextApiResponse } from "next"
 
 type Data = {
   message: string
 }
 
-const secret = process.env.SANITY_SECRET as string;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   if (req.method !== "POST") {
     console.error("Must be a POST request")
     return res.status(401).json({ message: "Must be a POST request" })
-  }
-
-  if (!isValidRequest(req, secret)) {
-    console.log("Invalid request")
-    res.status(401).json({ message: "Invalid signature" })
-    return
   }
 
   try {
