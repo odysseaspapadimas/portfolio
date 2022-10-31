582c954b65683d56d21b706b3e8edaf2e5699684
odysseaspapadimas <odysseas.patra@gmail.com>
2022-11-01 00:24:21 +0200

    revalidation rewrite

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,30 +1,26 @@
-import type { NextApiRequest, NextApiResponse } from "next"
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  message: string
-}
+  message: string;
+};
 
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  if (req.method !== "POST") {
-    console.error("Must be a POST request")
-    return res.status(401).json({ message: "Must be a POST request" })
-  }
-
-  try {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method == "POST") {
     const {
       body: { type, slug },
-    } = req
+    } = req;
 
-    switch (type) {
-      case "post":
-        await res.revalidate(`/post/${slug}`)
-        console.log(`Revalidated /post/${slug} with type ${type}`)
-        return res.json({ message: `Revalidated "${type}" with slug "${slug}"` })
-    }
+    console.log(type, slug)
 
-    return res.json({ message: "No managed type" })
-  } catch (err) {
-    return res.status(500).send({ message: "Error revalidating" })
+    await res.revalidate('/blog')
+    await res.revalidate(`/post/${slug}`);
+    console.log(`Revalidated /post/${slug} with type ${type}`);
+    return res.json({ message: `Revalidated "${type}" with slug "${slug}"` });
+  } else {
+    console.error("Must be a POST request");
+    return res.status(401).json({ message: "Must be a POST request" });
   }
 }
