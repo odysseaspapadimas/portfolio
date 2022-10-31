b25a536b5f6b65b8e4894fd3ba187d76426bedf3
odysseaspapadimas <odysseas.patra@gmail.com>
2022-10-31 23:24:27 +0200

    more vercel functions debugging

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -35,7 +35,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths: paths.map((slug: any) => ({ params: { slug } })),
-        fallback: false
+        fallback: true
     };
 }
 
